13d28397bed1ad726d504deb92fdaace1a2a7054
vinicius kremer <vinicius.kremer@gmail.com>
2021-03-03 15:31:42 -0300

    Final aula 4

--- script.js
+++ script.js
@@ -12,61 +12,64 @@ const Modal = {
                 .classList
                 .remove("active");
         }
-    },
-    open() {
-        // Abrir modal
-        // Adicionar a classe "active" ao modal
-        document
-            .querySelector('.modal-overlay')
-            .classList
-            .add("active");
-    },
-    close() {
-        // Fechar modal
-        // Retirar a classe "active" do modal
-        document
-            .querySelector(".modal-overlay")
-            .classList
-            .remove("active");
     }
 };
 
-// Minha lista de objetos de transações
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    },
-    {
-        id: 2,
-        description: 'Criação website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        id: 1,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021'
-    }
-];
-
-// Meu objeto da transação
 const Transaction = {
+    all: transactions = [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Criação website',
+            amount: 500000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Internet',
+            amount: -20000,
+            date: '23/01/2021'
+        }
+    ],
+    add(transaction) {
+        Transaction.all.push(transaction)
+
+        App.reload();
+    },
+    remove(index) {
+        Transaction.all.splice(index, 1);
+
+        App.reload();
+    },
     incomes() {
-        // Somar todas as entradas
+        let income = 0;
+
+        Transaction.all.forEach((transaction) => {
+            if(transaction.amount > 0) {
+                income += transaction.amount;
+            }
+        });
+
+        return income;
     },
     expenses() {
-        // Somar todas as saídas
+        let expanse = 0;
+
+        Transaction.all.forEach((transaction) => {
+            if(transaction.amount < 0) {
+                expanse += transaction.amount;
+            }
+        });
+
+        return expanse;
     },
     total() {
-        // Total = entradas - saídas
+        return Transaction.incomes() + Transaction.expenses();
     }
 };
 
-// Substituir os dados do HTML com os dados do JS
 const DOM = {
     transactionContainer: document.querySelector("#data-table tbody"),
     addTransaction(transaction, index) {
@@ -89,6 +92,20 @@ const DOM = {
         `;
 
         return html;
+    },
+    updateBalance() {
+        document
+            .getElementById("incomeDisplay")
+            .innerHTML = Utils.formatCurrency(Transaction.incomes());
+        document
+            .getElementById("expanseDisplay")
+            .innerHTML = Utils.formatCurrency(Transaction.expenses());
+        document
+            .getElementById("totalDisplay")
+            .innerHTML = Utils.formatCurrency(Transaction.total());
+    },
+    clearTransactions() {
+        DOM.transactionContainer.innerHTML = "";
     }
 }
 
@@ -102,9 +119,82 @@ const Utils = {
             currency: "BRL"
         })
         return signal + value;
+    },
+    formatAmount(value) {
+        value = Number(value) * 100;
+        return value;
+    },
+    formatDate(date) {
+        const splittedDate = date.split("-");
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
     }
 }
 
-transactions.forEach((transction) => {
-    DOM.addTransaction(transction);
-})
+const Form = {
+    description: document.querySelector("input#description"),
+    amount: document.querySelector("input#amount"),
+    date: document.querySelector("input#date"),
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+    validateFields(){
+        const { description, amount, date } = Form.getValues();
+
+        if(description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+            throw new Error("Preencha todos os dados corretamente.")
+        }
+    },
+    formatData() {
+        let { description, amount, date } = Form.getValues();
+
+        amount = Utils.formatAmount(amount);
+        date = Utils.formatDate(date);
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+    save(transaction) {
+        Transaction.add(transaction);
+    },
+    clearFields() {
+        Form.description.value = "";
+        Form.amount.value = "";
+        Form.date.value = "";
+    },
+    submit(event) {
+        event.preventDefault();
+
+        try {
+            Form.validateFields();
+            const transaction = Form.formatData();
+            Form.save(transaction);
+            Form.clearFields();
+            Modal.toggle();
+        } catch (e) {
+            alert(e.message);
+        }
+    }
+}
+
+const App = {
+    init() {
+        Transaction.all.forEach((transction) => {
+            DOM.addTransaction(transction);
+        });
+
+        DOM.updateBalance();
+    },
+    reload() {
+        DOM.clearTransactions();
+        App.init();
+    }
+}
+
+App.init();
